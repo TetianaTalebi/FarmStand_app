0e97bebcc06f129b61cd5aa2380af1d323d0cdf8
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-07-12 22:22:03 -0400

    Require Product model into seeds.js

--- seeds.js
+++ seeds.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Product = require('./models/product');
 
 mongoose.connection.on('connected', () => console.log('connected'));
 mongoose.connection.on('open', () => console.log('open'));
@@ -8,3 +9,4 @@ mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
 mongoose.connection.on('close', () => console.log('close'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/FarmStandApp');
+
