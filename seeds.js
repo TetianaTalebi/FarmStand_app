256f6aa219f71edd807fa16e076b56182b34e524
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-07-12 22:41:20 -0400

    Create an array of objects seedProducts in seeds.js

--- seeds.js
+++ seeds.js
@@ -10,6 +10,49 @@ mongoose.connection.on('close', () => console.log('close'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/FarmStandApp');
 
+const seedProducts = [
+    {    name: 'Red Apple',
+        price: 0.50,
+        category: 'fruit'
+    },
+    {    name: 'Yellow Apple',
+        price: 0.60,
+        category: 'fruit'
+    },
+    {    name: 'Potato',
+        price: 0.18,
+        category: 'vegetable'
+    },
+    {    name: 'Carrot',
+        price: 0.25,
+        category: 'vegetable'
+    },
+    {    name: 'Onion',
+        price: 0.30,
+        category: 'vegetable'
+    },
+    {    name: 'Garlic',
+        price: 0.10,
+        category: 'vegetable'
+    },
+    {    name: 'Cow Milk',
+        price: 1.50,
+        category: 'dairy'
+    },
+    {    name: 'Cream',
+        price: 2.10,
+        category: 'dairy'
+    },
+    {    name: 'Orange',
+        price: 1.80,
+        category: 'fruit'
+    },
+    {    name: 'Pepper',
+        price: 1.64,
+        category: 'vegetable'
+    }
+];
+
 // const p = new Product ({
 //     name: 'Ruby Grapefruit', 
 //     price: 1.99,
