f6320f97481a5f660ead25732ebff6977271308a
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-10 00:00:57 -0400

    Create a shortcut for mongoose.Schema in models/farm.js

--- models/farm.js
+++ models/farm.js
@@ -1,2 +1,5 @@
 const mongoose = require('mongoose');
 
+// Destructuring Schema from mongoose object
+// Creating a shortcut for mongoose.Schema
+const {Schema} = mongoose; // This line of code is equal to: const Schema = mongoose.Schema;
