d559bbc580a8e01c505641a2be0c1c944802e063
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-10 00:09:27 -0400

    Define a simple model for a farm in models/farm.js

--- models/farm.js
+++ models/farm.js
@@ -3,3 +3,18 @@ const mongoose = require('mongoose');
 // Destructuring Schema from mongoose object
 // Creating a shortcut for mongoose.Schema
 const {Schema} = mongoose; // This line of code is equal to: const Schema = mongoose.Schema;
+
+// Defining a basic model for a farm
+const farmSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Farm must have a name!!!']
+    },
+    city: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: [true, 'E-mail is required!!!']
+    }
+});
