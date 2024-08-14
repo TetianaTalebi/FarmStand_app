1b5572ff89e3d2f26db23452d2bc8db78d4d0503
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-14 00:24:37 -0400

    Revise the code in the route app.post(/farms/:id/products... in index.js

--- index.js
+++ index.js
@@ -123,8 +123,19 @@ app.get('/farms/:id/products/new', (req, res) => {
 })
 
 app.post('/farms/:id/products', async (req, res) => {
-    res.send(req.body);
+    const {id} = req.params;
+    const farm = await Farm.findById(id);
+    
+    const {name, price, category} = req.body;
+    const product = new Product({name, price, category});
 
+    farm.products.push(product);
+    product.farm = farm;
+
+    await farm.save();
+    await product.save();
+    
+    res.redirect(`/farms/${farm._id}`);
 })
 
 app.listen(3000, (req, res) =>{
